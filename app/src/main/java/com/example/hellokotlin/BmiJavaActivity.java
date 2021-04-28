ae6c690811e16b486c500b940ab911e829dfede5
ekdnlt <ekdnlt1532@naver.com>
2021-04-28 11:45:23 +0900

    BMI by Jav & Kotlin

--- app/src/main/java/com/example/hellokotlin/BmiJavaActivity.java
+++ app/src/main/java/com/example/hellokotlin/BmiJavaActivity.java
@@ -3,12 +3,46 @@ package com.example.hellokotlin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class BmiJavaActivity extends AppCompatActivity {
+    EditText heightField, weightField;
+    TextView txtResult;
+    Button btnBMI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_view_binding);
+
+        heightField = findViewById(R.id.heightField);
+        weightField = findViewById(R.id.weightField);
+        txtResult = findViewById(R.id.textResult);
+        btnBMI = findViewById(R.id.btnBmi);
+
+        btnBMI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String sheight = heightField.getText().toString();
+                String sweight = weightField.getText().toString();
+//                try {
+//                    double bmi = Double.parseDouble(sweight) / Math.pow(Double.parseDouble(sheight) / 100.0,2.0);
+//                    txtResult.setText("Your BMI = "+bmi);
+//                } catch (Exception e){
+//                    txtResult.setText("Input(s) missing");
+//                }
+                if(sheight.isEmpty() || sweight.isEmpty()){
+                    double bmi = Double.parseDouble(sweight) / Math.pow(Double.parseDouble(sheight) / 100.0,2.0);
+                    txtResult.setText("Your BMI = "+bmi);
+                }
+                else{
+                    txtResult.setText("Input(s) missing");
+                }
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/hellokotlin/BmiKotlinActivity.kt
+++ app/src/main/java/com/example/hellokotlin/BmiKotlinActivity.kt
@@ -2,10 +2,34 @@ package com.example.hellokotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import java.lang.Exception
 
 class BmiKotlinActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_view_binding)
+
+        val heightField = findViewById<EditText>(R.id.heightField);
+        val weightField = findViewById<EditText>(R.id.weightField);
+        val txtResult = findViewById<TextView>(R.id.textResult);
+        val btnBMI = findViewById<Button>(R.id.btnBmi);
+
+        btnBMI.setOnClickListener {
+//            val height = heightField.getText().toString().toDouble()
+//            val weight = weightField.getText().toString().toDouble()
+            val sheight = heightField.getText().toString()
+            val sweight = weightField.getText().toString()
+
+            try {
+                val bmi = sweight.toDouble() /Math.pow(sheight.toDouble()/100.0,2.0)
+                txtResult.setText("Your BMI = "+bmi)
+            }catch (e : Exception){
+                txtResult.setText("Input(s) missing")
+            }
+
+        }
     }
 }
