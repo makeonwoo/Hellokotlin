6bbaf0afb332ce52476bef2fe988a2d45050cc07
ekdnlt <ekdnlt1532@naver.com>
2021-05-06 14:31:54 +0900

    Flow Control in Java

--- app/src/main/java/com/example/hellokotlin/MainActivity.kt
+++ app/src/main/java/com/example/hellokotlin/MainActivity.kt
@@ -19,6 +19,9 @@ class MainActivity : AppCompatActivity() {
         val btnVarJava = findViewById<Button>(R.id.btnVarJava)
         val btnVarKotlin = findViewById<Button>(R.id.btnVarKotlin)
 
+        val btnFlowControlJava = findViewById<Button>(R.id.btnFlowControlJava)
+        val btnFlowControlKotlin = findViewById<Button>(R.id.btnFlowControlKotlin)
+
         btnJava.setOnClickListener {
             startActivity(Intent(this@MainActivity,BmiJavaActivity::class.java));
         }
@@ -31,5 +34,11 @@ class MainActivity : AppCompatActivity() {
         btnVarKotlin.setOnClickListener {
             startActivity(Intent(this@MainActivity,VariableKotlinActivity::class.java))
         }
+        btnFlowControlJava.setOnClickListener {
+            startActivity(Intent(this@MainActivity,FlowControlJavaActivity::class.java))
         }
-   }
+        btnFlowControlKotlin.setOnClickListener {
+            startActivity(Intent(this@MainActivity,FlowControlKotlinActivity::class.java))
+        }
+    }
+}
